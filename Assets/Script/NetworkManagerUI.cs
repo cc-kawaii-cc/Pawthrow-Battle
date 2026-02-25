b70d001a049bf3d60ff6adf6c430fe57a833a669
KRICHZ-SPEC <thanakrit.chang@bumail.net>
2026-02-25 22:24:42 +0700

    recode

--- Assets/Script/NetworkManagerUI.cs
+++ Assets/Script/NetworkManagerUI.cs
@@ -2,33 +2,32 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NetworkManagerUI : MonoBehaviour {
+public class NetworkManagerUI : MonoBehaviour 
+{
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button clientBtn;
-
-    private void Awake() {
-        // เมื่อกดปุ่ม Host ให้เริ่มเป็น Host
-        hostBtn.onClick.AddListener(() => {
+    private void Awake() 
+    {
+        hostBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartHost();
             HideUI();
         });
-
-        // เมื่อกดปุ่ม Server ให้เริ่มเป็น Server
-        serverBtn.onClick.AddListener(() => {
+        serverBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartServer();
             HideUI();
         });
-
-        // เมื่อกดปุ่ม Client ให้เริ่มเป็น Client (เข้าจอยคนอื่น)
-        clientBtn.onClick.AddListener(() => {
+        
+        clientBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartClient();
             HideUI();
         });
     }
-
-    private void HideUI() {
-        // เมื่อเริ่มเกมแล้ว ให้ซ่อนปุ่มพวกนี้ไป
+    private void HideUI() 
+    {
         gameObject.SetActive(false);
     }
 }
